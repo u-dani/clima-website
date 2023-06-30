9a880e47e9256165eca86b9b04796a16bbd58b71
u-dani <danibezsouza@gmail.com>
2023-06-29 22:21:07 -0300

    initial settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary-light": "#EC6316",
+        "secondary-light": "#F5F5F5",
+
+        "primary-dark": "#8A2BE2",
+        "secondary-dark": "#1B1A1D",
+
+        "light-gray": "#A4A4A4",
+        "medium-gray": "#222",
+        "dark-gray": "#101014",
       },
     },
   },
   plugins: [],
-}
+};
