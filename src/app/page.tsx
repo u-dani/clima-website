8e7becbe37a12fe25bf79f0316a030bf8b70def7
u-dani <danibezsouza@gmail.com>
2023-06-30 20:35:35 -0300

    SearchInput created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { ThemeToggle } from '@/components/ThemeToggle'
-
 export default function Home() {
-    return (
-        <main className='bg-white dark:bg-black dark:text-white'>
-            homepage
-            <ThemeToggle />
-        </main>
-    )
+    return <main></main>
 }
---
+++ src/components/form/SearchInput.tsx
@@ -0,0 +1,20 @@
+import { MdSearch } from 'react-icons/md'
+
+interface ISearchInputProps {
+    id: string
+    placeholder?: string
+}
+
+export const SearchInput = ({ id, placeholder }: ISearchInputProps) => {
+    return (
+        <label htmlFor={id} className='flex relative'>
+            <input
+                type='text'
+                placeholder={placeholder}
+                id={id}
+                className='rounded-xl text-sm md:text-base border border-transparent bg-secondary-light px-4 py-2 pr-10 w-full outline-none text-medium-gray focus:bg-transparent focus:border-primary-light focus:dark:border-primary-dark'
+            />
+            <MdSearch className='text-xl md:text-2xl absolute top-2/4 -translate-y-2/4 right-3 md:right-3 text-primary-light dark:text-primary-dark' />
+        </label>
+    )
+}
