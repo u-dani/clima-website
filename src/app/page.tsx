b5fdef8bf19c117e78db9ed69f8f834cf948f039
u-dani <danibezsouza@gmail.com>
2023-07-02 03:17:00 -0300

    cleaning up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { Header } from '@/components/layout/Header'
-
 export default function Home() {
-    return (
-        <main>
-            <Header />
-        </main>
-    )
+    return <main></main>
 }
