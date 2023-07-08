3ecf074b669ee8fb626cfee0afe6531dcdfbb348
u-dani <danibezsouza@gmail.com>
2023-07-07 21:36:40 -0300

    att forecast page

--- src/app/(pages)/forecast/page.tsx
+++ src/app/(pages)/forecast/page.tsx
@@ -2,5 +2,14 @@ import { requestGeocoding } from '@/request/requestGeocoding'
 import { requestWeatherForecast } from '@/request/requestWeatherForecast'
 
 export default async function WeatherForecastPage() {
+    const geocodingData = await requestGeocoding({ city: 'Salgueiro' })
+
+    const weatherDataList = geocodingData
+        ? await requestWeatherForecast({
+              lat: geocodingData.lat,
+              lon: geocodingData.lon,
+          })
+        : undefined
+
     return <main></main>
 }
