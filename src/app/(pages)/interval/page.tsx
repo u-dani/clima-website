a2397b708afc0002b9e415feefe1fc08082a084a
u-dani <danibezsouza@gmail.com>
2023-07-12 15:54:51 -0300

    weather forecast request on the interval page done

--- src/app/(pages)/interval/page.tsx
+++ src/app/(pages)/interval/page.tsx
@@ -1,3 +1,44 @@
-export default function WeatherForecastWithIntervalsPage() {
-  return <main></main>;
+import { requestGeocoding } from '@/request/requestGeocoding'
+import { requestWeatherForecast } from '@/request/requestWeatherForecast'
+
+export default async function WeatherForecastWithIntervalsPage({
+    searchParams,
+}: {
+    searchParams: { city: string; unit: string }
+}) {
+    const geocodingData = await requestGeocoding({
+        city: searchParams.city ?? 'Brasília',
+    })
+
+    const weatherData = geocodingData
+        ? await requestWeatherForecast({
+              lat: geocodingData.lat,
+              lon: geocodingData.lon,
+          })
+        : undefined
+
+    const weatherDataList: (typeof weatherData)[] = []
+    let arr: typeof weatherData = []
+    weatherData?.forEach(data => {
+        const dataDate = new Date(data.dt * 1000).getDate()
+        const arrDate =
+            arr?.length !== 0 && arr
+                ? new Date(arr[0].dt * 1000).getDate()
+                : undefined
+
+        if (dataDate === new Date().getDate()) {
+            return
+        }
+
+        if (arrDate === undefined || arrDate === dataDate) {
+            arr?.push(data)
+            return
+        }
+
+        weatherDataList.push(arr)
+        arr = [data]
+    })
+    weatherDataList.push(arr)
+
+    return <main></main>
 }
