007bef76ef7d452dc224c9a37ee3d84d099e0d42
u-dani <danibezsouza@gmail.com>
2023-07-12 15:04:09 -0300

    att requestWeatherForecast

--- src/request/requestWeatherForecast.ts
+++ src/request/requestWeatherForecast.ts
@@ -4,7 +4,7 @@ import {
     IWeatherDataResponse,
 } from './data'
 
-interface IWeatherForecastRequestParameters extends IWeatherDataResponse {
+interface IWeatherForecastRequestResponse extends IWeatherDataResponse {
     pop: number
 }
 
@@ -14,7 +14,7 @@ export const requestWeatherForecast = async ({
     lang = 'pt_br',
     units = 'metric',
 }: IWeatherDataRequestParameters): Promise<
-    IWeatherForecastRequestParameters[]
+    IWeatherForecastRequestResponse[]
 > => {
     const res = await fetch(
         `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&lang=${lang}&appid=${APIKEY}`
