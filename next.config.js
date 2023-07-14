e40123ab11179c2e15fed1794c868cd0a6b63feb
u-dani <danibezsouza@gmail.com>
2023-07-14 14:53:44 -0300

    att  next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    headers: () => [
-        {
-            source: '/:path*',
-            headers: [
-                {
-                    key: 'Cache-Control',
-                    value: 'no-store',
-                },
-            ],
-        },
-    ],
-}
+const nextConfig = {}
 
 module.exports = nextConfig
