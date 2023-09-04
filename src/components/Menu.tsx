82e82d69adbe94282148c3297bae13db3de24e5f
u-dani <danibezsouza@gmail.com>
2023-09-04 03:11:16 -0300

    changing the html structure

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -13,6 +13,12 @@ export const Menu = (themeToggleProps: IThemeToggleProps) => {
   const searchParams = useSearchParams()
   const [showMenu, setShowMenu] = useState(false)
 
+  const bodyElement = document.querySelector('body')
+
+  showMenu
+    ? (bodyElement!.style.overflow = 'hidden')
+    : (bodyElement!.style.overflow = 'auto')
+
   return (
     <div className='cursor-pointer text-base text-secondary-light'>
       <MdMenuOpen
@@ -23,7 +29,7 @@ export const Menu = (themeToggleProps: IThemeToggleProps) => {
       />
 
       {showMenu && (
-        <div className='absolute left-0 top-0 z-50 flex h-screen w-screen flex-col items-center bg-dark-gray p-4'>
+        <div className='fixed left-0 top-0 z-50 flex h-screen w-full flex-col items-center bg-dark-gray p-4'>
           <div className='flex self-end'>
             <MdClose
               className='text-3xl hover:text-primary-light dark:hover:text-primary-dark'
@@ -33,60 +39,62 @@ export const Menu = (themeToggleProps: IThemeToggleProps) => {
             />
           </div>
 
-          <div className='mt-8 flex min-w-[320px] flex-col items-center gap-4 p-4'>
-            <div className='w-full border-b border-light-gray p-4 text-center'>
-              <h3>Menu</h3>
-            </div>
+          <div className='mt-10 flex w-[360px] flex-col gap-10'>
+            <section className='flex flex-col gap-2'>
+              <div className='w-full border-b border-light-gray p-4 text-center'>
+                <h3>Menu</h3>
+              </div>
 
-            <div
-              className='flex w-full'
-              onClick={() => {
-                setShowMenu(false)
-              }}>
-              <NextLink
-                path='/current'
-                href={`/current?${searchParams.toString()}`}
-                className='w-full text-white'>
-                Clima Atual
-              </NextLink>
-            </div>
+              <div
+                className='flex w-full'
+                onClick={() => {
+                  setShowMenu(false)
+                }}>
+                <NextLink
+                  path='/current'
+                  href={`/current?${searchParams.toString()}`}
+                  className='w-full text-white'>
+                  Clima Atual
+                </NextLink>
+              </div>
 
-            <div
-              className='flex w-full'
-              onClick={() => {
-                setShowMenu(false)
-              }}>
-              <NextLink
-                path='/forecast'
-                href={`/forecast?${searchParams.toString()}`}
-                className='w-full text-white'>
-                Próximos Dias
-              </NextLink>
-            </div>
+              <div
+                className='flex w-full'
+                onClick={() => {
+                  setShowMenu(false)
+                }}>
+                <NextLink
+                  path='/forecast'
+                  href={`/forecast?${searchParams.toString()}`}
+                  className='w-full text-white'>
+                  Próximos Dias
+                </NextLink>
+              </div>
 
-            <div
-              className='flex w-full'
-              onClick={() => {
-                setShowMenu(false)
-              }}>
-              <NextLink
-                path='/interval'
-                href={`/interval?${searchParams.toString()}`}
-                className='w-full text-white'>
-                Intervalo de 3 horas
-              </NextLink>
-            </div>
-          </div>
+              <div
+                className='flex w-full'
+                onClick={() => {
+                  setShowMenu(false)
+                }}>
+                <NextLink
+                  path='/interval'
+                  href={`/interval?${searchParams.toString()}`}
+                  className='w-full text-white'>
+                  Intervalo de 3 horas
+                </NextLink>
+              </div>
+            </section>
 
-          <div className='mt-16 flex min-w-[320px] flex-col items-center gap-4 p-4'>
-            <div className='w-full border-b border-light-gray p-4 text-center'>
-              <h3>Outras opções</h3>
-            </div>
+            <section className='flex flex-col gap-2'>
+              <div className='w-full border-b border-light-gray p-4 text-center'>
+                <h3>Outras opções</h3>
+              </div>
 
-            <div className='flex w-full justify-between p-2'>
-              <ScaleTemperatureForm />
-              <ThemeToggle {...themeToggleProps} />
-            </div>
+              <div className='flex w-full justify-between p-2'>
+                <ScaleTemperatureForm />
+                <ThemeToggle {...themeToggleProps} />
+              </div>
+            </section>
           </div>
         </div>
       )}
